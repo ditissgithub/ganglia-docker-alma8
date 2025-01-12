adb3fe6ab3c8892ef99add21bd1e8ee220e1172d
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2025-01-12 21:04:55 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,12 @@ else
     # Check if required environment variables are set
     required_vars=( ROOT_PASSWD GANGLIA_USER GANGLIA_USER_PASSWD PORT GRIDNAME CLUSTERNAME HOSTIP ) 
 
+    for var in "${required_vars[@]}"; do
+       if [ -z "${!var}" ]; then
+         echo "Error: Environment variable $var is not set." >&2
+         exit 1
+      fi
+    done
 
     # Replace the default HTTP port in the Apache configuration
     if [[ -n "$PORT" ]]; then
