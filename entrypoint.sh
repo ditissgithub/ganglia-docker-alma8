0b4fcafa1156094817d9836b798298b15b1ac550
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2025-01-12 20:29:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,16 @@ else
     envsubst < /ganglia_conf/gmetad.conf.template > /etc/ganglia/gmetad.conf
     envsubst < /ganglia_conf/gmond.conf.template > /etc/ganglia/gmond.conf
     cp -r /ganglia_conf/ganglia.conf /etc/httpd/conf.d/ganglia.conf
+
+    groupadd -r ganglia_user && useradd -r -g ganglia_user ganglia_user
+    echo "ganglia_user:guser@@123" | chpasswd
+    sed -i '34i Cmnd_Alias GANG_SERVICES = /usr/sbin/httpd, /usr/sbin/gmetad, /usr/sbin/gmond, /entrypoint.sh' /etc/sudoers
+    sed -i '101i ganglia_user ALL=(ALL) NOPASSWD: GANG_SERVICES' /etc/sudoers
     # Start supervisord to manage services
     echo "Starting supervisord..."
     exec /usr/bin/supervisord -c /etc/supervisord.conf
     touch /root/CONFIGURED
+    su - ganglia_user
 
 fi
    
