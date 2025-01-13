facf3fef3efd0941965b3e3b2c23e6c70e0f42b8
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2025-01-14 03:49:58 +0530

    Update run.sh

--- portable/run.sh
+++ portable/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+if ! docker images | awk '{print $1}' | grep  ganglia; then
+  docker load < cdac_ganglia.img
+else
+  echo "Image 'ganglia' already exists."
+fi
+
 export $(cat ./.env) > /dev/null 2>&1;docker-compose up -d
